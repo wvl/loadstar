7800e617c69c94ae8420f3fd0e182e9307f0aad5
Wayne Larsen <wayne@larsen.st>
2014-06-12 19:40:12 -0400

    Example: Use server to host dev/prod builds

--- example/server.js
+++ example/server.js
@@ -1,13 +1,36 @@
+var fs = require('fs');
+var path = require('path');
 var express = require('express');
+var ejs = require('ejs');
 var server = module.exports = express();
 server.use(express.static(__dirname + '/www'));
 var app = require('./app');
+var bldr = require('..');
 
 var user = new ex.models.User({name: 'bldr'});
-console.log(user.say('Hi Node!'));
+var dev = process.env.NODE_ENV !== 'production';
+console.log(user.say('Hi Node!'), dev);
 
-// Add this route in dev mode, to serve content from your dev directory
-server.use('/dev', express.static(__dirname));
+if (dev) {
+  // Add this route in dev mode, to serve content from your dev directory
+  server.use('/dev', express.static(__dirname));
+}
+
+server.get('/', function(req, res) {
+  var layout = fs.readFileSync(path.join(__dirname, 'www', 'layout.html'), 'utf8');
+  var sources = dev ? ['app_shim'] : ['deps','app'];
+  var exConfig = JSON.stringify({init: 'app'});
+  var fn = ejs.compile(layout);
+  res.send(fn({exConfig: exConfig, sources: sources}));
+});
+
+server.get('/admin', function(req, res) {
+  var layout = fs.readFileSync(path.join(__dirname, 'www', 'layout.html'), 'utf8');
+  var sources = dev ? ['admin_shim'] : ['deps','app','admin'];
+  var exConfig = JSON.stringify({init: 'admin'});
+  var fn = ejs.compile(layout);
+  res.send(fn({exConfig: exConfig, sources: sources}));
+});
 
 if (require.main === module) {
   server.listen(3000, function() {
